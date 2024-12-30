53451172d057574f6c11067a2e6c0db1a77293f9
minhkakart <nguyenminh2732@gmail.com>
2024-12-30 12:14:26 +0700

    refactor

--- src/main/java/com/minhkakart/bigdata/TrainCassandraJob.java
+++ src/main/java/com/minhkakart/bigdata/TrainCassandraJob.java
@@ -8,20 +8,19 @@ import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-public class TrainJob extends Configured implements Tool {
+public class TrainCassandraJob extends Configured implements Tool {
     public static void main(String[] args) throws Exception {
-        ToolRunner.run(new Configuration(), new TrainJob(), args);
+        ToolRunner.run(new Configuration(), new TrainCassandraJob(), args);
     }
 
     @Override
     public int run(String[] args) throws Exception {
         Configuration conf = getConf();
-        String input = conf.get("input");
+//        String input = conf.get("input");
         String output = conf.get("output");
         /* Configuration parameters
         // Configurations for Random Forest
@@ -36,17 +35,19 @@ public class TrainJob extends Configured implements Tool {
         String cassandra_input_columnfamily = conf.get("cassandra.input.columnfamily");
         String cassandra_input_datacenter = conf.set("cassandra.input.datacenter", "datacenter1");
         */
-        
+
         Job job = Job.getInstance(conf, "Random Forest");
 
-        job.setJarByClass(TrainJob.class);
+        job.setJarByClass(TrainCassandraJob.class);
         job.setMapperClass(RandomForestTrainMapper.class);
         job.setReducerClass(RandomForestTrainReducer.class);
 
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
 
-        FileInputFormat.addInputPath(job, new Path(input));
+        job.setInputFormatClass(PlayerStatsInputFormat.class);
+
+//        FileInputFormat.addInputPath(job, new Path(input));
         FileOutputFormat.setOutputPath(job, new Path(output));
 
         return job.waitForCompletion(true) ? 0 : 1;
