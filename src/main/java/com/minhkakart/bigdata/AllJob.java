6e19ca55d6db32d0134db2420e665575aa4cb404
minhkakart <nguyenminh2732@gmail.com>
2025-01-15 22:53:55 +0700

    Ok

--- src/main/java/com/minhkakart/bigdata/AllJob.java
+++ src/main/java/com/minhkakart/bigdata/AllJob.java
@@ -6,20 +6,26 @@ import org.apache.hadoop.util.ToolRunner;
 public class AllJob {
     public static void main(String[] args) throws Exception {
         Configuration configuration = new Configuration();
-            int trainJobStatus = ToolRunner.run(configuration, new TrainCassandraJob(), args);
-            if (trainJobStatus != 0) {
-                System.err.println("Train job failed");
-                System.exit(trainJobStatus);
-            }
-            int testJobStatus = ToolRunner.run(configuration, new TestCassandraJob(), args);
-            if (testJobStatus != 0) {
-                System.err.println("Test job failed");
-                System.exit(testJobStatus);
-            }
-            int scoreJobStatus = ToolRunner.run(configuration, new ScoreJob(), args);
-            if (scoreJobStatus != 0) {
-                System.err.println("Score job failed");
-                System.exit(scoreJobStatus);
-            }
+        System.out.println("Running train job");
+        int trainJobStatus = ToolRunner.run(configuration, new TrainCassandraJob(), args);
+        if (trainJobStatus != 0) {
+            System.err.println("Train job failed");
+            System.exit(trainJobStatus);
+        }
+        System.err.println("Train job succeeded");
+        System.out.println("Running test job");
+        int testJobStatus = ToolRunner.run(configuration, new TestCassandraJob(), args);
+        if (testJobStatus != 0) {
+            System.err.println("Test job failed");
+            System.exit(testJobStatus);
+        }
+        System.err.println("Test job succeeded");
+        System.out.println("Running score job");
+        int scoreJobStatus = ToolRunner.run(configuration, new ScoreJob(), args);
+        if (scoreJobStatus != 0) {
+            System.err.println("Score job failed");
+            System.exit(scoreJobStatus);
+        }
+        System.err.println("Score job succeeded");
     }
 }
